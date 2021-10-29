76edda0a4881cca5afef39c03aacc3bc7726677d
Sean_or_Tony <anthonyasamoah48@gmail.com>
2021-10-29 18:17:37 +0000

    including AnimeTracker

---
+++ AnimeTrackerV2.py
@@ -0,0 +1,36 @@
+# simple python app to manage anime
+
+
+import os
+CurrEpp = 17
+file = 'Watch Baby Steps 2nd Season Episode ' + str(CurrEpp) + '.mp4'
+
+# Location of the anime to be viewed must be updated after each season!
+# ----------------------------------------------------
+os.chdir("C:\\Users\\dd\\Videos\\Baby Steps\\Season 2")
+# ----------------------------------------------------
+
+# This shows the episode & show that is going to be played,
+# remember to update after each episode!!!
+
+# TODO : include code to display current time, right on this line
+
+print('\nCurrent Anime: Baby Steps',
+	  "\n-------------------------\n",
+	  'Current Episode: ',
+	  CurrEpp,
+	  "\n-------------------------\n" )
+
+print('hit enter to continue')
+pause = input()
+# !!!!!!!!!!Next Episode:  !!!!!!!!!!!!!!!!!
+
+# The launch function of this program, also remember to update!!!!!!
+# TODO : include code to launch the anime directory
+# TODO : include code to launch the anime tracker in vs code or any text editor.
+# @notepad.exe .\\Anime_Tracker.bat %
+# ================================================
+os.execvp(file, )
+# ================================================
+
+CurrEpp += 1
