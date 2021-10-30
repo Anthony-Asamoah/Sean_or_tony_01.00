abf8235f78e4a184caf86c3e4722ea6089a791a1
Sean_or_Tony <anthonyasamoah48@gmail.com>
2021-10-30 17:20:14 +0000

    Foodie.py completed

---
+++ Foodie.py
@@ -0,0 +1,56 @@
+# a program to chose what food to eat based on the time of day
+
+from random import randint
+from datetime import datetime
+
+time = datetime.now()  # Create a datetime object to get the hour of day
+hour = time.hour  # variable to store the hour of day
+
+# Dictionary to hold meals and their times
+MainMenu = {
+	"Breakfast": ['Bread & Egg', 'Kooko', 'Waakye/Jollof'],
+	"Brunch": ['Waakye/Jollof', 'G)b3', 'Koliko'],
+	"Lunch": ['Waakye/Jollof', 'G)b3', 'Koliko', 'Banku', 'Fufu'],
+	"Dinner": ['Indomie', 'Fried Rice', 'Kenkey', 'Jollof/Waakye', 'Banku'],
+	"Extra": ['Bread & Egg', 'Waakye/Jollof', 'Indomie']
+}
+
+# List to store meals only available depending on the time of day
+available = []
+if hour in range(6, 10):
+	mealtime = 'Breakfast'
+	available += MainMenu.get(mealtime)
+elif hour in range(10, 13):
+	mealtime = 'Brunch'
+	available += MainMenu.get(mealtime)
+elif hour in range(13, 18):
+	mealtime = 'Lunch'
+	available += MainMenu.get(mealtime)
+elif hour in range(18, 21):
+	mealtime = 'Dinner'
+	available += MainMenu.get(mealtime)
+elif hour in range(21, 24):
+	mealtime = 'Extra'
+	available += MainMenu.get(mealtime)
+else:
+	mealtime = 'Fasting!'
+	available.append(mealtime)
+
+# Variable to store suggested meal
+suggestion1 = available[randint(0, len(available) - 1)]
+suggestion2 = available[randint(0, len(available) - 1)]
+
+# To ensure both suggestions are not the same
+while suggestion2 == suggestion1:
+	suggestion2 = available[randint(0, len(available) - 1)]
+
+# variable to hold the output statement, this is just for output formatting purposes
+sentence = f'  It is time for {mealtime}. Thus, i suggest {suggestion1}, or some {suggestion2}.'
+
+# Give output
+print(
+	'\n',
+	' Something to eat? '.center(len(sentence), '/'),
+	f'\n\n{sentence}',
+	f'\n', ''.center(len(sentence), '_')
+	)
