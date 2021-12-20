93e22e0c7e1e9d2daada0ec43ead93d8bdf63def
Sean_or_Tony <anthonyasamoah48@gmail.com>
2021-12-20 09:43:46 +0000

    Specified a dir for saving shelve file - multiclipboard

--- PythonChapt2.py
+++ PythonChapt2.py
@@ -402,13 +402,14 @@
 
 # import shelve, pyperclip, sys, os
 #
-# dir = 'C\\users\\dd\\desktop\\mcbshelf'
-# mcbShelf = shelve.open('mcb')
-
+# dir = 'C:\\users\\dd\\desktop\\mcbshelf'  # shelve directory
+# os.mkdir(dir)
+# mcbShelf = shelve.open(dir)
+#
 # if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
 # 	mcbShelf[sys.argv[2]] = pyperclip.paste()
 # elif len(sys.argv) == 2:
-
+#
 # 	if sys.argv[1].lower() == 'list':
 # 		pyperclip.copy(str(list(mcbShelf.keys())))
 # 	elif sys.argv[1] in mcbShelf:
