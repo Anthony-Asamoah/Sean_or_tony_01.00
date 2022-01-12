a5a7961df8e8099a4564f09912cc8924e2f22a7f
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-01-12 15:03:54 +0000

    Finished Debugging(assertions and logging)

--- PythonChapt2.py
+++ PythonChapt2.py
@@ -394,7 +394,7 @@
 # 	quizFile.close()
 # 	answerFile.close()
 
-#! python3
+
 # mcb.pyw - Saves and loads pieces of text to the clipboard.
 # Usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword.
 # py.exe mcb.pyw <keyword> - Loads keyword to clipboard.
@@ -417,4 +417,74 @@
 #
 # mcbShelf.close()
 
+#import logging
 
+
+# def boxPrint(symbol, width, height):
+# 	if len(symbol) != 1:
+# 		raise Exception('Symbol must be a single character string.')
+# 	if width <= 2:
+# 		raise Exception('Width must be greater than 2.')
+# 	if height <= 2:
+# 		raise Exception('Height must be greater than 2.')
+
+# 	print(symbol * width)
+# 	for i in range(height - 2):
+# 		print(symbol + (' ' * (width - 2)) + symbol)
+# 		print(symbol * width)
+
+
+# for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+# 	try:
+# 		boxPrint(sym, w, h)
+# 	except Exception as err:
+# 		print('An exception happened: ' + str(err))
+
+
+# logging.basicConfig(filename="C:/users/dd/desktop/loggingtemp1.txt", level=logging.DEBUG, format='%(asctime)s -%(
+# levelname)s - %(message)s') logging.debug('Start of program')
+
+
+# def factorial(n):
+# 	logging.debug(f'Start of factorial <{n}>')
+# 	total = 1
+# 	for i in range(1, n + 1):
+# 		total *= i
+# 		logging.debug('i is ' + str(i) + ', total is ' + str(total))
+# 	logging.debug(f'End of factorial <{n}>')
+# 	return total
+
+
+# print(factorial(20))
+# logging.debug('End of program')
+
+# import random, logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# guess = ''
+# while guess.lower() not in ['heads', 'tails', 'h', 't']:
+# 	print('Guess the coin toss! Enter heads or tails:')
+# 	guess = input()
+#
+# if guess in ['heads', 'h']:
+# 	guess = 1
+# else:
+# 	guess = 0
+#
+# toss = random.randint(0, 1) # 0 is tails, 1 is heads
+# logging.debug(f'{toss} as val')
+#
+# if toss == guess:
+# 	print('You got it!')
+# else:
+# 	print('Nope! Guess again!')
+# 	guess = input()
+#
+# 	if guess in ['heads', 'h']:
+# 		guess = 1
+# 	else:
+# 		guess = 0
+# 	toss = random.randint(0, 1)
+# 	if toss == guess:
+# 		print('You got it!')
+# 	else:
+# 		print('Nope. You are really bad at this game.')
